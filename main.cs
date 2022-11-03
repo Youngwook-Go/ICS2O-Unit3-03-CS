7b734a90086ebc734ff857719db3f0bb03fb0de3
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-11-03 01:37:45 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+// Created by: Youngwook
+// Created on: OCT 2022
+//
+// This function gets volume of pyramid
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //this function accept user input
+        double sphereRadius;
+        double sphereVolume;
+
+        Console.WriteLine("This program gets volume of sphere.");
+        Console.WriteLine("Please enter the value of the sphere.\n");
+        Console.Write("Enter value of sphere radius : "); sphereRadius = Convert.ToInt32(Console.ReadLine());
+
+        sphereVolume = Math.PI * Math.Pow(sphereRadius, 3) * 4 / 3;
+        
+        // this function prints the result
+        Console.WriteLine("\nVolume of the pyramid is : " + sphereVolume.ToString("0.00") + " cm² ");
+        Console.WriteLine("\ndone.");
+    }
+}
